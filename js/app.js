8af5f9e7e820ac759580f7137cae2997d52902a3
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-11 13:42:32 -0400

    dandole formato de moneda y porcentaje a el cabecero

--- js/app.js
+++ js/app.js
@@ -1,38 +1,56 @@
 const ingresos = [
-    new Ingreso('Salario', 2100.00),
-    new Ingreso('Venta coche', 1500)
+  new Ingreso("Salario", 2100.0),
+  new Ingreso("Venta coche", 1500),
 ];
 
 const egresos = [
-    new Egreso('Renta departamento', 900),
-    new Egreso('Ropa', 400)
+  new Egreso("Renta departamento", 900),
+  new Egreso("Ropa", 400),
 ];
 
-let cargarApp = ()=>{
-    cargarCabecero();
-}
+let cargarApp = () => {
+  cargarCabecero();
+};
 
-let totalIngresos = ()=>{
-    let totalIngreso = 0;
-    for(let ingreso of ingresos){
-        totalIngreso += ingreso.valor;
-    }
-    return totalIngreso;
-}
+let totalIngresos = () => {
+  let totalIngreso = 0;
+  for (let ingreso of ingresos) {
+    totalIngreso += ingreso.valor;
+  }
+  return totalIngreso;
+};
 
-let totalEgresos = ()=>{
-    let totalEgreso = 0;
-    for(let egreso of egresos){
-        totalEgreso += egreso.valor;
-    }
-    return totalEgreso;
-}
+let totalEgresos = () => {
+  let totalEgreso = 0;
+  for (let egreso of egresos) {
+    totalEgreso += egreso.valor;
+  }
+  return totalEgreso;
+};
 
-let cargarCabecero = ()=>{
-    let presupuesto = totalIngresos() - totalEgresos();
-    let porcentajeEgreso = totalEgresos()/totalIngresos();
-    document.getElementById('presupuesto').innerHTML = presupuesto;
-    document.getElementById('porcentaje').innerHTML = porcentajeEgreso;
-    document.getElementById('ingresos').innerHTML = totalIngresos();
-    document.getElementById('egresos').innerHTML = totalEgresos();
-}
+let cargarCabecero = () => {
+  let presupuesto = totalIngresos() - totalEgresos();
+  let porcentajeEgreso = totalEgresos() / totalIngresos();
+  document.getElementById("presupuesto").innerHTML = formatoMoneda(presupuesto);
+  document.getElementById("porcentaje").innerHTML =
+    formatoPorcentaje(porcentajeEgreso);
+  document.getElementById("ingresos").innerHTML = formatoMoneda(
+    totalIngresos()
+  );
+  document.getElementById("egresos").innerHTML = formatoMoneda(totalEgresos());
+};
+
+const formatoMoneda = (valor) => {
+  return valor.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+};
+
+const formatoPorcentaje = (valor) => {
+  return valor.toLocaleString("en-US", {
+    style: "percent",
+    minimumFractionDigits: 2,
+  });
+};
