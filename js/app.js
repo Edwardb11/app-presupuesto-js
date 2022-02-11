5de70058f1a8534749faab5107001804388c6334
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-11 14:23:22 -0400

    cargando los egresos con el DOM en js

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ const egresos = [
 let cargarApp = () => {
   cargarCabecero();
   cargarIngresos();
+  cargarEgresos();
 };
 
 let totalIngresos = () => {
@@ -79,4 +80,32 @@ const crearIngresoHTML = (ingreso) => {
 </div>
     `;
   return ingresoHTML;
+};
+
+const cargarEgresos = () => {
+  let egresosHTML = "";
+  for (let egreso of egresos) {
+    egresosHTML += crearEgresoHTML(egreso);
+  }
+  document.getElementById("lista-egresos").innerHTML = egresosHTML;
+};
+
+const crearEgresoHTML = (egreso) => {
+  let egresoHTML = `
+    <div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${egreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">- ${formatoMoneda(egreso.valor)}</div>
+        <div class="elemento_porcentaje">${formatoPorcentaje(
+          egreso.valor / totalEgresos()
+        )}</div>
+        <div class="elemento_eliminar">
+            <button class='elemento_eliminar--btn'>
+                <ion-icon name="close-circle-outline"></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>
+    `;
+  return egresoHTML;
 };
