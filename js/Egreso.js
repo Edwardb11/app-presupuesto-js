8523239e55d6653cdddae136ecc78272c6d04c31
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-11 13:18:52 -0400

    clases de ingreso y egreso

---
+++ js/Egreso.js
@@ -0,0 +1,10 @@
+class Egreso extends Dato {
+  static contadorEgreso = 0;
+  constructor(descripcion, valor) {
+    super(descripcion, valor);
+    this._id = ++Egreso.contadorEgreso;
+  }
+  get id() {
+    return this._id;
+  }
+}
