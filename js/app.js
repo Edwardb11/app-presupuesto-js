1acbc2e0161bf72fd0981aa118d8f1617f587ba8
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-11 13:24:52 -0400

    agregando id y funcion para actualizar e; presupuesto ingreso y egreso

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const ingresos = [
+  new Ingreso("Salario", 2100.0),
+  new Ingreso("Nuevo Coche", 1500.0),
+];
+
+const Egresos = [
+  new Egreso("Salario", 2100.0),
+  new Egreso("Nuevo Coche", 1500.0),
+];
+
+let cargarApp = () => {
+  cargarCabecero();
+};
+
+let cargarCabecero = () => {};
